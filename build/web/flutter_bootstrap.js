6a89695d0050aee45c654a4486acad45ebe6f14e
Hoang Hung <hunghoang00a@gmail.com>
2026-06-09 18:57:23 +0700

    web build for Vercel deploy

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "770030110" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2207595505" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
