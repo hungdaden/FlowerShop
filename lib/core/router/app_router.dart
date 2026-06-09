73f88441055c7701731c553514953fd215ad0261
Hoang Hung <hunghoang00a@gmail.com>
2026-06-09 17:00:10 +0700

    feat: support admin- prefix for Vercel app subdomains

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -45,7 +45,7 @@ CustomTransitionPage<void> _buildPageTransition({
 bool get isAdminSubdomain {
   if (kIsWeb) {
     final host = Uri.base.host;
-    return host.startsWith('admin.');
+    return host.startsWith('admin.') || host.startsWith('admin-');
   }
   return false;
 }
